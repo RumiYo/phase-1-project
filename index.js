41921ce63bda4ec6fdab8769cb88ab9c3967c5f8
RumiYo <yokoyamarumi@gmail.com>
2023-09-20 00:47:07 -0400

    test API

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-document.addEventListener('DOMContentLoaded', () => console.log('We are connected!'))
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('We are connected!');
+    fetchDataWithDrinkName();
+})
+
+function fetchDataWithDrinkName(){
+    fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
+    .then(resp => resp.json())
+    .then(data => console.log(data))
+}
