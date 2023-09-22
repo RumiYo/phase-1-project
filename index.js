7ff551ae813111896e964e92c2f25fdc4ab5d696
RumiYo <yokoyamarumi@gmail.com>
2023-09-22 16:12:29 -0400

    Display cocktails with cocktail name searches

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
         e.preventDefault()
         const cocktailName = e.target.cocktailName.value;
         fetchDataWithDrinkName(cocktailName);
+
     })
 })
 
@@ -16,9 +17,25 @@ function fetchDataWithDrinkName(name){
     .then(resp => resp.json())
     .then(data => {
         const cocktailsArr = data.drinks;
-        console.log(cocktailsArr)
+        console.log(cocktailsArr);
+        displayDrinkList(cocktailsArr)
     })
 }
 
+//Display list of cocktails
+function displayDrinkList(arr){
+    document.querySelector('#SearchResult').innerHTML = '';
+    arr.forEach(element => {
+            console.log(element.strDrink,element.strDrinkThumb);
+            const card = document.createElement('div');
+            card.className = 'cocktailThumbnail'
+            card.innerHTML = `
+                <img src=${element.strDrinkThumb} >
+                <p>${element.strDrink}</p>
+            `
+            document.querySelector('#SearchResult').appendChild(card)
+        }
+    )   
+}
 
 
