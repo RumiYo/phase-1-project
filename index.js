a6f382d1953461aae593aacfa232809a3d5b1336
RumiYo <yokoyamarumi@gmail.com>
2023-09-22 15:02:38 -0400

    collect input value from cocktail name and call API

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('We are connected!');
-    fetchDataWithDrinkName();
+
+    //get cocktail input value and get data 
+    const form = document.querySelector('#NameSearch');
+    form.addEventListener('submit',(e) =>{
+        e.preventDefault()
+        const cocktailName = e.target.cocktailName.value;
+        fetchDataWithDrinkName(cocktailName);
+    })
 })
 
-function fetchDataWithDrinkName(){
-    fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
+//Call API to get cocktaillist from input cocktail name
+function fetchDataWithDrinkName(name){
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`)
     .then(resp => resp.json())
-    .then(data => console.log(data))
+    .then(data => {
+        const cocktailsArr = data.drinks;
+        console.log(cocktailsArr)
+    })
 }
+
+
+
