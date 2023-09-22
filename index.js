979e5ed02d30ec04a4dd952ac2b7fcc761301eb8
RumiYo <yokoyamarumi@gmail.com>
2023-09-22 16:42:09 -0400

    get input for ingredient search, fetch data, adnd display them

--- index.js
+++ index.js
@@ -2,18 +2,39 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log('We are connected!');
 
     //get cocktail input value and get data 
-    const form = document.querySelector('#NameSearch');
-    form.addEventListener('submit',(e) =>{
+    const cocktailNameForm = document.querySelector('#NameSearch');
+    cocktailNameForm.addEventListener('submit',(e) =>{
         e.preventDefault()
-        const cocktailName = e.target.cocktailName.value;
+        const cocktailName = e.target.CocktailName.value;
         fetchDataWithDrinkName(cocktailName);
+        document.querySelector('#IngredientName').value = '';
+    })
 
+    //get ingredient input value and get data 
+    const ingredientForm = document.querySelector('#IngredientSearch');
+    console.log(ingredientForm)
+    ingredientForm.addEventListener('submit',(e) =>{
+        e.preventDefault()
+        const ingredient = e.target.IngredientName.value;
+        fetchDataWithIngredientName(ingredient);
+        document.querySelector('#CocktailName').value = '';
     })
 })
 
 //Call API to get cocktaillist from input cocktail name
 function fetchDataWithDrinkName(name){
     fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`)
+    .then(resp => resp.json())
+    .then(data => {
+        const cocktailsArr = data.drinks;
+        console.log(cocktailsArr);
+        displayDrinkList(cocktailsArr)
+    })
+}
+
+//Call API to get cocktaillist from input ingredient name
+function fetchDataWithIngredientName(name){
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${name}`)
     .then(resp => resp.json())
     .then(data => {
         const cocktailsArr = data.drinks;
